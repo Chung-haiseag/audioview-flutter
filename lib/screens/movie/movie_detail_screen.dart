9fcd9a405c2fb6b02af75337b29bcc5ec52074b5
Chung-haiseag <chs9182@gmail.com>
2026-02-05 23:48:35 +0900

    Fix: Add missing rendering.dart import to MovieDetailScreen

--- lib/screens/movie/movie_detail_screen.dart
+++ lib/screens/movie/movie_detail_screen.dart
@@ -1,5 +1,6 @@
 ﻿import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'dart:ui';
